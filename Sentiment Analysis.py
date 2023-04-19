55672244f4c17631e2dcfd532d13c056cf4c9cae
Yousha <123200960+You-sha@users.noreply.github.com>
2023-04-20 01:40:11 +0530

    Added comments

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -8,10 +8,11 @@ Created on Thu Apr 20 01:16:41 2023
 import pandas as pd
 df = pd.read_csv('edited_tweets.csv')
 df.head()
-df.drop(df.columns[0], axis=1, inplace=True)
+df.drop(df.columns[0], axis=1, inplace=True) #removing "Unnamed:0" column
 
+# Using vader SA
 from nltk.sentiment import SentimentIntensityAnalyzer
-from tqdm import tqdm
+from tqdm import tqdm # To visualize ETA
 
 sia = SentimentIntensityAnalyzer()
 
@@ -23,9 +24,10 @@ for i, row in tqdm(df.iterrows(), total=len(df)):
     
 sa = pd.DataFrame(res).T
 sa = sa.reset_index()
-sa = sa.rename(columns={'index':'id'})
+sa = sa.rename(columns={'index':'id'}) # for merging
 sa_df = sa.merge(df, on='id',how='right')
 
+# Reordering columns to get the SA scores at the end
 sa_df = sa_df[['id','date','content','username','likeCount','retweetCount','neg','neu','pos','compound']]
 
 sa_df.to_csv('sentiment_analyzed_tweets.csv', index=None)
